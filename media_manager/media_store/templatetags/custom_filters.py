6c61d8a4e38a5d9226a3b838882d243862dced7f
kushal <kushalpoudel2058@gmail.com>
2025-01-04 14:40:32 +0545

    refactor: removed unsed  file

--- media_manager/media_store/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import os
-
-register = template.Library()
-
-@register.filter
-def splitext(value):
-    """Split the file path into base name and extension."""
-    return os.path.splitext(value)[-1].lstrip('.')
